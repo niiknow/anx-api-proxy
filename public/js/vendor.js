43a78f25a7eeda52e951295f2479b48820e34290
noogen <tom@noogen.net>
2018-12-16 07:01:54 -0600

    app updated

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -1,2 +1,2 @@
-webpackJsonp([2],{1:function(n,o,e){n.exports=e("lRwf")},lRwf:function(n,o){n.exports=Vue}},[1]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1:function(n,o,w){n.exports=w("i7/w")},"i7/w":function(n,o){n.exports=Vue}},[[1,0]]]);
 //# sourceMappingURL=vendor.js.map
