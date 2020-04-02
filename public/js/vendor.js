dd09eaa69f9957aff830380366c53b08bf70f878
noogen <tom@noogen.net>
2020-04-02 11:15:14 -0500

    update package

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -1,27 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/vendor"],{
-
-/***/ 1:
-/*!*****************!*\
-  !*** multi vue ***!
-  \*****************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(/*! vue */"vue");
-
-
-/***/ }),
-
-/***/ "vue":
-/*!**********************!*\
-  !*** external "Vue" ***!
-  \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = Vue;
-
-/***/ })
-
-},[[1,"/js/manifest"]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1:function(n,o,w){n.exports=w("i7/w")},"i7/w":function(n,o){n.exports=Vue}},[[1,0]]]);
 //# sourceMappingURL=vendor.js.map
